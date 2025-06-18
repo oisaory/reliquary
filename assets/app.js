78cbf92dd5788f7b22fce778f7190f5ab568f02a
CesarScur <csrscr@gmail.com>
2025-06-18 03:40:56 +0000

    Add `bootstrap` JavaScript import to enable full Bootstrap functionality.

--- assets/app.js
+++ assets/app.js
@@ -7,5 +7,6 @@ import './bootstrap.js';
  */
 import './styles/app.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
