91b629b3b4364bc9e048c6634e1caa00b453c142
Cesar Scur <csrscr@gmail.com>
2025-07-16 22:19:38 -0300

    fix(mailer): update email sender address in registration confirmation
    - Change sender address to `no-reply@santasreliquias.com.br` for consistency.

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -41,7 +41,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('mailer@your-domain.com', 'Reliquary'))
+                    ->from(new Address('no-reply@santasreliquias.com.br', 'Reliquary'))
                     ->to((string) $user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
