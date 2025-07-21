73482fe588d4d7a9765c94cf139dee61678a6b40
Cesar Scur <csrscr@gmail.com>
2025-07-20 22:27:33 -0300

    feat(forms): enhance password validation rules for registration and change password forms
    - Increased minimum password length to 8 characters.
    - Added `NotCompromisedPassword` constraint to prevent usage of leaked passwords.
    - Enhanced security by requiring at least a weak password strength using the `PasswordStrength` constraint.

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -12,6 +12,8 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\NotCompromisedPassword;
+use Symfony\Component\Validator\Constraints\PasswordStrength;
 
 class RegistrationFormType extends AbstractType
 {
@@ -38,11 +40,15 @@ class RegistrationFormType extends AbstractType
                         'message' => 'Please enter a password',
                     ]),
                     new Length([
-                        'min' => 6,
+                        'min' => 8,
                         'minMessage' => 'Your password should be at least {{ limit }} characters',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
+                    new PasswordStrength([
+                        'minScore' => PasswordStrength::STRENGTH_WEAK,
+                    ]),
+                    new NotCompromisedPassword(),
                 ],
             ])
         ;
