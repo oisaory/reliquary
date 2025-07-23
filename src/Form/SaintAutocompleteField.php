3d7e945e7ffe88dac12f2c1413dffcf68d778039
Cesar Scur <csrscr@gmail.com>
2025-07-22 21:45:14 -0300

    feat(form): translate saint autocomplete placeholder text
    - Injected `TranslatorInterface` into `SaintAutocompleteField` to handle translations.
    - Updated placeholder to use translated string `saint.autocomplete.placeholder`.
    - Added placeholder translation to `saint.pt_BR.yaml`.

--- src/Form/SaintAutocompleteField.php
+++ src/Form/SaintAutocompleteField.php
@@ -6,17 +6,25 @@ use App\Entity\Saint;
 use App\Repository\SaintRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Contracts\Translation\TranslatorInterface;
 use Symfony\UX\Autocomplete\Form\AsEntityAutocompleteField;
 use Symfony\UX\Autocomplete\Form\BaseEntityAutocompleteType;
 
 #[AsEntityAutocompleteField]
 class SaintAutocompleteField extends AbstractType
 {
+    private TranslatorInterface $translator;
+
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'class' => Saint::class,
-            'placeholder' => 'Start typing to search for a saint...',
+            'placeholder' => $this->translator->trans('saint.autocomplete.placeholder', [], 'saint'),
             'choice_label' => 'name',
             'searchable_fields' => ['name'],
             'security' => false,
